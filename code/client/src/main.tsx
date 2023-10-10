9a3ed9bbfc71938c921eb1449932eae79137d3ef
Piotr Droś <piotr.dros01@gmail.com>
2023-10-10 12:56:07 +0200

    chore: setup root layout

--- code/client/src/main.tsx
+++ code/client/src/main.tsx
@@ -1,4 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Root from './routes/Root'
 
-ReactDOM.createRoot(document.getElementById('root')!).render(<React.StrictMode></React.StrictMode>)
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Root />,
+  },
+])
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+)
