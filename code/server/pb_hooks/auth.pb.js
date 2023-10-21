035fc5e5df5989d49b645243a45364f9906dfe42
Piotr Droś <piotr.dros01@gmail.com>
2023-10-21 16:43:02 +0200

    chore: do not require verification for now

--- code/server/pb_hooks/auth.pb.js
+++ code/server/pb_hooks/auth.pb.js
@@ -1,11 +1,9 @@
 /// <reference path="../pb_data/types.d.ts" />
 
 onRecordAuthRequest((e) => {
-  const user = e.record;
-
-  const isVerified = user.verified();
-
-  if (!isVerified) {
-    throw new Error('User is not verified');
-  }
+  // const user = e.record;
+  // const isVerified = user.verified();
+  // if (!isVerified) {
+  //   throw new Error('User is not verified');
+  // }
 }, 'users');
