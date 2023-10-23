a9eb8803a81f21013984f76ca0bc0876fd69892f
Piotr Droś <piotr.dros01@gmail.com>
2023-10-23 17:08:11 +0200

    chore: add data to repo

---
+++ code/client/src/pages/protected/Courses/components/CourseCard.tsx
@@ -0,0 +1,29 @@
+import { CoursesResponse } from '@/types/pocketbase-types'
+import { Badge, Card, CardBody, CardHeader, Heading, Text } from '@chakra-ui/react'
+
+interface Props {
+  course: CoursesResponse
+}
+
+function CourseCard({ course }: Props) {
+  return (
+    <Card
+      h={'full'}
+      size={'sm'}
+      _hover={{ transform: 'scale(1.02)' }}
+      transition={'150ms'}
+      bgColor={!course.isFaculty ? 'purple.100' : 'gray.200'}>
+      <CardHeader>
+        {course.isFaculty ? <Badge>Faculty</Badge> : <Badge>Obligatory</Badge>}
+        <Heading as={'h3'} size={'md'} mt={2}>
+          {course.name}
+        </Heading>
+      </CardHeader>
+      <CardBody>
+        <Text noOfLines={3}>{course.description}</Text>
+      </CardBody>
+    </Card>
+  )
+}
+
+export default CourseCard
