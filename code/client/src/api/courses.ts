9ad8b09c0851111c8f8f431eb387cffbb6667ef3
Piotr Droś <piotr.dros01@gmail.com>
2023-10-25 23:15:14 +0200

    chore: generate types

--- code/client/src/api/courses.ts
+++ code/client/src/api/courses.ts
@@ -2,7 +2,7 @@ import { pb } from '@/config/pocketbase'
 import { CoursesResponse } from '@/types/pocketbase-types'
 import { wait } from '@/utils/helpers'
 
-export async function getCourses() {
+export async function getCourses(): Promise<CoursesResponse[]> {
   await wait(3000)
   return await pb.collection('courses').getFullList<CoursesResponse>()
 }
--- code/client/src/types/pocketbase-types.ts
+++ code/client/src/types/pocketbase-types.ts
@@ -3,7 +3,11 @@
  */
 
 export enum Collections {
+  Comments = 'comments',
   Courses = 'courses',
+  Fields = 'fields',
+  Reviews = 'reviews',
+  SubComments = 'sub_comments',
   Users = 'users',
 }
 
@@ -31,35 +35,80 @@ export type AuthSystemFields<T = never> = {
 
 // Record types for each collection
 
-export interface CoursesRecord {
-  description: string
-  ects: number
-  field?: string
-  isOptional?: boolean
-  name: string
-  semester: number
+export interface CommentsRecord {
+  author: RecordIdString
+  content?: string
+  review: RecordIdString
 }
 
+export enum CoursesSeasonOptions {
+  'summer' = 'summer',
+  'winter' = 'winter',
+}
+export interface CoursesRecord {
+  description: string
+  difficulty?: number
+  ects: number
+  fields?: RecordIdString[]
+  isOptional?: boolean
+  name: string
+  season?: CoursesSeasonOptions
+  semester?: number
+}
+
+export interface FieldsRecord {
+  courses?: RecordIdString[]
+  description: string
+  name: string
+}
+
+export interface ReviewsRecord {
+  author: RecordIdString
+  content?: string
+  course: RecordIdString
+}
+
+export interface SubCommentsRecord {
+  author: RecordIdString
+  comment: RecordIdString
+  content?: string
+}
+
+export enum UsersRoleOptions {
+  'admin' = 'admin',
+  'student' = 'student',
+  'guest' = 'guest',
+}
 export interface UsersRecord {
   avatar?: string
-  name?: string
+  name: string
+  role: UsersRoleOptions
 }
 
 // Response types include system fields and match responses from the PocketBase API
+export type CommentsResponse<Texpand = unknown> = Required<CommentsRecord> & BaseSystemFields<Texpand>
 export type CoursesResponse<Texpand = unknown> = Required<CoursesRecord> & BaseSystemFields<Texpand>
+export type FieldsResponse<Texpand = unknown> = Required<FieldsRecord> & BaseSystemFields<Texpand>
+export type ReviewsResponse<Texpand = unknown> = Required<ReviewsRecord> & BaseSystemFields<Texpand>
+export type SubCommentsResponse<Texpand = unknown> = Required<SubCommentsRecord> & BaseSystemFields<Texpand>
 export type UsersResponse<Texpand = unknown> = Required<UsersRecord> & AuthSystemFields<Texpand>
 
 // Types containing all Records and Responses, useful for creating typing helper functions
 
 export interface CollectionRecords {
+  comments: CommentsRecord
   courses: CoursesRecord
+  fields: FieldsRecord
+  reviews: ReviewsRecord
+  sub_comments: SubCommentsRecord
   users: UsersRecord
 }
 
 export interface CollectionResponses {
+  comments: CommentsResponse
   courses: CoursesResponse
+  fields: FieldsResponse
+  reviews: ReviewsResponse
+  sub_comments: SubCommentsResponse
   users: UsersResponse
 }
-
-// Type for usage with type asserted PocketBase instance
-// https://github.com/pocketbase/js-sdk#specify-typescript-definitions
