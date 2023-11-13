501b235c335a8c766f285021a8f7965d94d975b5
Piotr Droś <piotr.dros01@gmail.com>
2023-11-13 17:31:43 +0100

    refactor: not use magic numbers

--- code/client/src/pages/Courses/Courses.hooks.ts
+++ code/client/src/pages/Courses/Courses.hooks.ts
@@ -3,9 +3,11 @@ import { useFilterStore } from '@/stores/filtersStore'
 import { useDebounce } from '@uidotdev/usehooks'
 import { useQuery } from 'react-query'
 
+const DEBOUNCE_TIME = 1000
+
 export function useCourses() {
   const filters = useFilterStore((state) => state.filters)
-  const debouncedName = useDebounce(filters.name, 500)
+  const debouncedName = useDebounce(filters.name, DEBOUNCE_TIME)
   return useQuery(['courses', filters], () =>
     getFilteredCourses({
       filters: { ...filters, name: debouncedName },
